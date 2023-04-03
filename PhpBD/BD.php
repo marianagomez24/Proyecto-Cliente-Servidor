e30c6f5bae651598225d4b34ea0664a8db559c22
christ48 <111949820+christ48@users.noreply.github.com>
2023-04-02 19:00:00 -0600

    Add files via upload

--- PhpBD/BD.php
+++ PhpBD/BD.php
@@ -1,12 +1,10 @@
 <?php
-
-require("Conexion.php");
-
+session_start();
+require("conexion.php");
 
 if (!$conexion) {
     die("Error al conectar a la base de datos: " . mysqli_connect_error($conexion));
 }
-
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
@@ -15,52 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $correo = $_POST["correo"];
     $contrasena = $_POST["contrasena"];
 
-    $consulta = "SELECT * FROM usuarios WHERE correo = ?";
-    $stmt = mysqli_prepare($conexion, $consulta);
-    mysqli_stmt_bind_param($stmt, "s", $correo);
-    mysqli_stmt_execute($stmt);
-    $resultado = mysqli_stmt_get_result($stmt);
+    $sql = "SELECT * FROM usuarios WHERE correo='$correo'";
+    $result = mysqli_query($conexion, $sql);
+    $present = mysqli_num_rows($result);
 
-    if (mysqli_num_rows($resultado) > 0) {
-       
-        $response = array(
-            "success" => false,
-            "message" => "El correo ya está registrado en la base de datos"
-        );
+    if ($present > 0) {
+        $_SESSION['email_alert'] = '1';
+     
+        header("Location:http://localhost/Proyecto-Cliente-Servidor/registro.php");
     } else {
-       
         $contrasena_encriptada = password_hash($contrasena, PASSWORD_BCRYPT);
-
-       
         $consulta = mysqli_prepare($conexion, "INSERT INTO usuarios (nombre, apellido_paterno, apellido_materno, correo, contrasena) VALUES (?, ?, ?, ?, ?)");
         mysqli_stmt_bind_param($consulta, "sssss", $nombre, $apellido_paterno, $apellido_materno, $correo, $contrasena_encriptada);
 
-       
         if (mysqli_stmt_execute($consulta)) {
-           
-            $response = array(
-                "success" => true,
-                "message" => "Registro exitoso"
-            );
+           header("Location:http://localhost/Proyecto-Cliente-Servidor/login.html");
         } else {
-          
-            $response = array(
-                "success" => false,
-                "message" => "Error al registrar los datos: " . mysqli_error($conexion)
-            );
+            echo "Error al registrar los datos: " . mysqli_error($conexion);
         }
-
 
         mysqli_stmt_close($consulta);
     }
 }
-
-
-header('Content-Type: application/json');
-echo json_encode($response);
-
-
-mysqli_close($conexion);
 ?>
-
-
