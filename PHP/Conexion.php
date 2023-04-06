9294eb941fa09ee0588a5526f78d791109ff318b
marianagomez24 <marianagomez24123@gmail.com>
2023-04-06 11:00:30 -0600

    Cambios GENERALES
    Cambios en la organizacion de los ducmento y cambios en las paginas

---
+++ PHP/Conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+$servidor = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_de_datos = "gym_capital";
+
+
+$conexion = mysqli_connect($servidor, $usuario, $contrasena, $base_de_datos);
+?>
