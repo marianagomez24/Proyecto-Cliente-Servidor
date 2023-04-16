f6a3f6cd123bc6d53d060359678e04fa7574e6cd
christ48 <111949820+christ48@users.noreply.github.com>
2023-04-15 20:51:17 -0600

    Update DatosBD.php

--- PHP/DatosBD.php
+++ PHP/DatosBD.php
@@ -4,13 +4,14 @@ session_start();
 
 if(isset($_COOKIE['mi_sesion'])) {
     $correo = $_COOKIE['mi_sesion'];
-    // Luego puedes usar el valor de $correo para hacer una consulta a la base de datos y obtener los datos del usuario.
+    // Luego puedes usar el valor de $correo para hacer una consulta a la vista y obtener los datos del usuario.
     // Por ejemplo:
-    $consulta = mysqli_prepare($conexion, "SELECT nombre,apellido_paterno,apellido_materno,fecha_creacion FROM usuarios WHERE correo = ?");
+    $consulta = mysqli_prepare($conexion, "SELECT nombre,apellido_paterno,apellido_materno,fecha_creacion,membresia,correo FROM vista_usuarios WHERE correo = ?");
     mysqli_stmt_bind_param($consulta, "s", $correo);
     mysqli_stmt_execute($consulta);
-    mysqli_stmt_bind_result($consulta, $nombre, $apellido_paterno, $apellido_materno, $fecha_creacion);
+    mysqli_stmt_bind_result($consulta, $nombre, $apellido_paterno, $apellido_materno, $fecha_creacion, $membresia, $correo);
     mysqli_stmt_fetch($consulta);
 }
+
 ?>
 
